a0efd365bdcdfc9c09af922e0af77e968fbfee00
dayngovna <68909674+dayngovna@users.noreply.github.com>
2021-06-17 18:46:20 +0300

    Update mongoEbot.py

--- mongoEbot.py
+++ mongoEbot.py
@@ -16,7 +16,32 @@ async def on_ready():
         activity=discord.Game("ekfara bot")) 
 @client.command()
 async def create(ctx,years,floor,*,im):#создать анкету
+    if collection.count_documents({"_id":ctx.author.mention}) == 1:
+        collection.delete_one({"_id":ctx.author.mention})
     collection.insert_one({"_id":ctx.author.mention,'years': years,'floor': floor,'im': im,'ava': str(ctx.author.avatar_url)})
+
+    for x in bd:
+        if x['_id'] == ctx.author.mention:
+            embed = discord.Embed(title=f'Анкета '+x['_id'])
+            embed.set_thumbnail(url=x['ava'])
+            embed.add_field(name="Возраст",value=x['years'])
+            embed.add_field(name="Пол",value=x['floor'])
+            embed.add_field(name="О себе",value=x['im'],inline=False)
+            await ctx.author.send(embed=embed)
+@client.command()
+async def find(ctx,years):#поиск анкеты
+        for xx in bd:
+            print(xx)
+            if xx['years'] == str(years):
+                print("finded!")
+                embed = discord.Embed(title=f'Анкета '+xx['_id'])
+                embed.set_thumbnail(url=xx['ava'])
+                embed.add_field(name="Возраст",value=xx['years'])
+                embed.add_field(name="Пол",value=xx['floor'])
+                embed.add_field(name="О себе",value=xx['im'],inline=False)
+                await ctx.author.send(embed=embed)
+            else:
+                await ctx.author.send("Мы не нашли анкету,измените запрос")            
 @client.command()
 async def ekfar(ctx):#help
     embed = discord.Embed(title="Это бот знакомств от экфара")
