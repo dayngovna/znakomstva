09773b844f2712f052c1759b383d762a030e11ca
dayngovna <68909674+dayngovna@users.noreply.github.com>
2021-06-18 18:26:56 +0300

    Update mongoEbot.py

--- mongoEbot.py
+++ mongoEbot.py
@@ -15,9 +15,6 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online,
         activity=discord.Game("ekfara bot")) 
 @client.command()
-async def test(ctx):
-    await ctx.author.send(ctx.author.name)
-@client.command()
 async def create(ctx,years,floor,*,im):#создать анкету
     count = collection.count_documents({})
     if collection.count_documents({"_id":ctx.author.mention}) == 1:
@@ -63,7 +60,6 @@ async def ekfar(ctx):#help
     embed.add_field(name="Бот работает через лс",value="Просто напиши ему команду",inline=False)
     embed.add_field(name="Чтобы создать анкету пропиши",value=">create возраст пол текст",inline=False)
     embed.add_field(name="Чтобы найти случайную анкету пропиши",value=">random",inline=False)
-    embed.add_field(name="Чтобы найти анкету по возрасту пропиши",value=">find возраст",inline=False)
     embed.add_field(name="Чтобы вызвать это меню пропиши",value=">ekfar",inline=False)
     embed.add_field(name="Удачного пользования!",value="Создан тут https://discord.gg/sPruSKek2n",inline=False)
     await ctx.send(embed=embed)    
